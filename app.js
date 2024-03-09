cc6b8f68249033593db62f89ba3db68b1c3cc573
Axiom Days <tobeoke@gmail.com>
2024-03-09 17:57:44 +0100

    table functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const form = document.getElementById('regform');
 const xml = new XMLHttpRequest();
 const idtest = document.getElementById("test");
 const table = document.getElementById("table");
+let returnedForm = '';
 
 xml.open("GET", "https://student-biodata-api-e089235e13e4.herokuapp.com/api/biodata/");
 xml.send();
@@ -10,7 +11,7 @@ xml.responseType = "json";
 xml.onload = () => {
   if (xml.readyState == 4 && xml.status == 200) {
     console.log(xml.response);
-    const returnedForm = JSON.parse(xml.response);
+    const returnedForm = (xml.response);
     console.log(returnedForm[0]);
     console.log(returnedForm[0].first_name);
   } else {
